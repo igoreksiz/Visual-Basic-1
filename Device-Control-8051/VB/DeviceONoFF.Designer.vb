cc332a3b88f34d6950600172fbd147de043ac127
Alok Mistry <mistryalok@users.noreply.github.com>
2016-03-06 13:47:04 +0530

    Device control app
    Windows software to control your device using Microcontroller interface.

---
+++ Device-Control-8051/VB/DeviceONoFF.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbPort = New System.Windows.Forms.ComboBox()
+        Me.cmbBaud = New System.Windows.Forms.ComboBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.btnDisconnect = New System.Windows.Forms.Button()
+        Me.btnTurnon = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.btnTurnoff = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(87, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = " Com Port"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(87, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Baud Rate"
+        '
+        'cmbPort
+        '
+        Me.cmbPort.FormattingEnabled = True
+        Me.cmbPort.Location = New System.Drawing.Point(57, 25)
+        Me.cmbPort.Name = "cmbPort"
+        Me.cmbPort.Size = New System.Drawing.Size(121, 21)
+        Me.cmbPort.TabIndex = 2
+        '
+        'cmbBaud
+        '
+        Me.cmbBaud.FormattingEnabled = True
+        Me.cmbBaud.Location = New System.Drawing.Point(57, 65)
+        Me.cmbBaud.Name = "cmbBaud"
+        Me.cmbBaud.Size = New System.Drawing.Size(121, 21)
+        Me.cmbBaud.TabIndex = 3
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(195, 25)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 4
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnDisconnect
+        '
+        Me.btnDisconnect.Location = New System.Drawing.Point(195, 65)
+        Me.btnDisconnect.Name = "btnDisconnect"
+        Me.btnDisconnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisconnect.TabIndex = 5
+        Me.btnDisconnect.Text = "Disconnect"
+        Me.btnDisconnect.UseVisualStyleBackColor = True
+        '
+        'btnTurnon
+        '
+        Me.btnTurnon.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnTurnon.Location = New System.Drawing.Point(110, 135)
+        Me.btnTurnon.Name = "btnTurnon"
+        Me.btnTurnon.Size = New System.Drawing.Size(130, 56)
+        Me.btnTurnon.TabIndex = 8
+        Me.btnTurnon.Text = "Turn ON"
+        Me.btnTurnon.UseVisualStyleBackColor = False
+        '
+        'btnTurnoff
+        '
+        Me.btnTurnoff.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.btnTurnoff.Location = New System.Drawing.Point(110, 224)
+        Me.btnTurnoff.Name = "btnTurnoff"
+        Me.btnTurnoff.Size = New System.Drawing.Size(130, 56)
+        Me.btnTurnoff.TabIndex = 9
+        Me.btnTurnoff.Text = "Turn Off"
+        Me.btnTurnoff.UseVisualStyleBackColor = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(372, 344)
+        Me.Controls.Add(Me.btnTurnoff)
+        Me.Controls.Add(Me.btnTurnon)
+        Me.Controls.Add(Me.btnDisconnect)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.cmbBaud)
+        Me.Controls.Add(Me.cmbPort)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMain"
+        Me.Opacity = 0.8R
+        Me.Text = "Device Control"
+        Me.TransparencyKey = System.Drawing.Color.SlateGray
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cmbPort As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbBaud As System.Windows.Forms.ComboBox
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents btnDisconnect As System.Windows.Forms.Button
+    Friend WithEvents btnTurnon As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents btnTurnoff As System.Windows.Forms.Button
+
+End Class
---
+++ Device-Control-8051/VB/DeviceONoFF.vb
@@ -0,0 +1,107 @@
+﻿‘Serial Port Interfacing with VB.net 2010 Express Edition
+'Copyright (C) 2010  Richard Myrick T. Arellaga
+'
+'This program is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'This program is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with this program.  If not, see <http://www.gnu.org/licenses/&gt;.
+
+
+'### The original Code, serial Port Interfacing with VB.net was edited by Alok Mistry, to work with Microcontroller Projects.
+'### Configure your microcontroller to receive 'y' or 'n' to start and stop the device.
+'### if you have modified the code, redistribuite it, it would help others to do something more.
+'### Thanks to original author Richard Myrick.
+Imports System
+Imports System.ComponentModel
+Imports System.Threading
+Imports System.IO.Ports
+Public Class frmMain
+    Dim myPort As Array  'COM Ports detected on the system will be stored here
+    Delegate Sub SetTextCallback(ByVal [text] As String) 'Added to prevent threading errors during receiveing of data
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'When our form loads, auto detect all serial ports in the system and populate the cmbPort Combo box.
+        myPort = IO.Ports.SerialPort.GetPortNames() 'Get all com ports available
+        cmbBaud.Items.Add(9600)     'Populate the cmbBaud Combo box to common baud rates used
+        cmbBaud.Items.Add(19200)
+        cmbBaud.Items.Add(38400)
+        cmbBaud.Items.Add(57600)
+        cmbBaud.Items.Add(115200)
+
+        For i = 0 To UBound(myPort)
+            cmbPort.Items.Add(myPort(i))
+        Next
+        cmbPort.Text = "COM"    'Set cmbPort text to the first COM port detected
+        cmbBaud.Text = cmbBaud.Items.Item(0)    'Set cmbBaud text to the first Baud rate on the list
+
+        btnDisconnect.Enabled = False           'Initially Disconnect Button is Disabled
+
+    End Sub
+
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConnect.Click
+        SerialPort1.PortName = cmbPort.Text         'Set SerialPort1 to the selected COM port at startup
+        SerialPort1.BaudRate = cmbBaud.Text         'Set Baud rate to the selected value on
+
+        'Other Serial Port Property
+        SerialPort1.Parity = IO.Ports.Parity.None
+        SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.DataBits = 8            'Open our serial port
+        SerialPort1.Open()
+
+        btnConnect.Enabled = False          'Disable Connect button
+        btnDisconnect.Enabled = True        'and Enable Disconnect button
+
+    End Sub
+
+    Private Sub btnDisconnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDisconnect.Click
+        SerialPort1.Close()             'Close our Serial Port
+
+        btnConnect.Enabled = True
+        btnDisconnect.Enabled = False
+    End Sub
+
+    Private Sub btnSend_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTurnon.Click
+        SerialPort1.WriteLine("y")
+        btnTurnon.Enabled = False
+        btnTurnoff.Enabled = True
+        'The text contained in the txtText will be sent to the serial port as ascii
+        'plus the carriage return (Enter Key) the carriage return can be ommitted if the other end does not need it
+    End Sub
+
+    Private Sub btnTurnoff_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTurnoff.Click
+
+        SerialPort1.WriteLine("n")
+        btnTurnoff.Enabled = False
+        btnTurnon.Enabled = True
+        'The text contained in the txtText will be sent to the serial port as ascii
+        'plus the carriage return (Enter Key) the carriage return can be ommitted if the other end does not need it
+    End Sub
+
+    Private Sub cmbPort_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbPort.SelectedIndexChanged
+        If SerialPort1.IsOpen = False Then
+            SerialPort1.PortName = cmbPort.Text         'pop a message box to user if he is changing ports
+        Else                                                                               'without disconnecting first.
+            MsgBox("Valid only if port is Closed", vbCritical)
+        End If
+    End Sub
+
+    Private Sub cmbBaud_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbBaud.SelectedIndexChanged
+        If SerialPort1.IsOpen = False Then
+            SerialPort1.BaudRate = cmbBaud.Text         'pop a message box to user if he is changing baud rate
+        Else                                                                                'without disconnecting first.
+            MsgBox("Valid only if port is Closed", vbCritical)
+        End If
+    End Sub
+
+   
+   
+   
+End Class
